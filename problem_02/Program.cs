7b6525e74cf171dbcf88f4ec92a2b396cc122899
USER <USER@DESKTOP-KCU37G9>
2023-09-21 18:36:22 -0500

    Agregar archivos de proyecto.

---
+++ problem_02/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace problem_02
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            float baseNumero, exponente, resultado;
+
+            Console.Write("Ingrese la base: ");
+            baseNumero = float.Parse(Console.ReadLine());
+
+            Console.Write("Ingrese el exponente: ");
+            exponente = float.Parse(Console.ReadLine());
+
+            if (exponente >= 0)
+            {
+                resultado = 1;
+
+                for (int i = 0; i < exponente; i++)
+                {
+                    resultado *= baseNumero;
+                }
+
+                Console.Write("La potencia  es: " + resultado);
+            }
+            else
+            {
+                Console.Write("El exponente debe ser mayor o igual a cero.");
+            }
+
+            Console.ReadKey();
+        }
+    }
+    
+}
